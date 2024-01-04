43308b71515f58db90fdce0fe68494894aafe517
Yesenia <ymartinezac@uoc.edu>
2024-01-04 03:39:53 +0100

    blog done

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      'yellow': '#F7E733',
+      'green': '#324835',
+      'white': '#F7F7F9',
+      'blue': '#00E0D5',
+    },
+    fontFamily: {
+      heading: ['Vina Sans', 'sans-serif'],
+      body: ['Noto Sans', 'serif'],
+      button: ['Ubuntu', 'sans-serif',]
+    },
+    screens: {
+      'md': '864px',
+      'lg': '1000px',
+    },
+
+  }
+}
